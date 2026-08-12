12e545ffef059374fa5ce29506a938da3c21d260
CyrilLagnien <lagniencyril@gmail.com>
2026-08-12 16:59:24 +0200

    final

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   get "restaurants/new", to: "restaurants#new"
   get "restaurants/:id", to: "restaurants#show", as: "restaurant"
   post "restaurants", to: "restaurants#create"
+
+  get "restaurants/:restaurant_id/reviews/new", to: "reviews#new", as: "new_restaurant_review"
+  post "restaurants/:restaurant_id/reviews", to: "reviews#create", as: "restaurant_reviews"
 end
