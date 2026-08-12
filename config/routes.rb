70b79163bd6c92ba6c6fe53f1514f546b91af421
CyrilLagnien <lagniencyril@gmail.com>
2026-08-12 16:24:40 +0200

    v8

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
   get "restaurants", to: "restaurants#index"
+  get "restaurants/new", to: "restaurants#new"
+  get "restaurants/:id", to: "restaurants#show", as: "restaurant"
+  post "restaurants", to: "restaurants#create"
 end
