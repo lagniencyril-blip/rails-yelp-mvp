7e3e6ff86fa9c76443757df1deb86f138ddbfd7a
CyrilLagnien <lagniencyril@gmail.com>
2026-08-12 15:15:40 +0200

    v3

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_08_12_104508) do
+ActiveRecord::Schema[8.1].define(version: 2026_08_12_105206) do
+  create_table "restaurants", force: :cascade do |t|
+    t.string "address"
+    t.string "category"
+    t.datetime "created_at", null: false
+    t.string "name"
+    t.string "phone_number"
+    t.datetime "updated_at", null: false
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.text "content"
     t.datetime "created_at", null: false
